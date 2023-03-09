6d5eb4f16294b9c56589205e150264e80331b5a3
Kelton M. Cumbe <nakatsinho@gmail.com>
2023-03-09 13:55:10 +0200

    Testing improvments

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+import sys
 
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
+args = sys.argv[1:]  # Get all arguments except for the script name
+num_args = len(args)
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+# Print the number of arguments and the list of arguments
+if num_args == 0:
+    print("0 arguments.")
+else:
+    plural = "" if num_args == 1 else "s"
+    print("{} argument{}:".format(num_args, plural))
+    for i, arg in enumerate(args):
+        print("{}: {}".format(i + 1, arg))
