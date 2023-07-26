fd76f71293debee76d1a07a6f7c052c02b89ce67
Kelton M. Cumbe <nakatsinho@gmail.com>
2023-07-26 15:06:31 +0200

    Feat: Adding new project to the scope

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,22 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+
+function readAndPrintFileContent(filePath) {
+  fs.readFile(filePath, 'utf8', function (error, content) {
+    if (error) {
+      console.error("Error occurred while reading the file:");
+      console.error(error);
+    } else {
+      console.log(content);
+    }
+  });
+}
+
+if (process.argv.length !== 3) {
+  console.error("Usage: node read_file.js <file_path>");
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+readAndPrintFileContent(filePath);
